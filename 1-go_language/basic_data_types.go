6145ec8476de048d3ba9c7c6fa2411f7c354ce50
Oleg Khudenko <ohuden@softserveinc.com>
2017-07-28 13:53:35 +0300

    added structure

---
+++ 1-go_language/basic_data_types.go
@@ -0,0 +1,17 @@
+//Basic data types in Go
+
+1. Integers:
+	signed integer:
+		int, int8, int16, int32, int64
+	unsigned integers:
+		uint, uint8, uint16, uint32, uint64
+2. Float:
+	float32, float64
+	Complex:
+		complex64, complex128
+
+3. String
+4. Logical types
+		&& И
+		|| ИЛИ
+		! НЕ
