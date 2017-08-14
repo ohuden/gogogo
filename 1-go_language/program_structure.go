396627359cc0e1fdf28bd525856f190c66add1c3
Oleg Khudenko <ohuden@softserveinc.com>
2017-08-14 13:38:08 +0300

    commit

--- 1-go_language/program_structure.go
+++ 1-go_language/program_structure.go
@@ -1,1 +1,5 @@
 //Program Structure
+
+1. Use camelcase in name declarations. There are 25 scpecial word like if and switch that are taken + predeclared names like int true etc.package 1-go_language
+
+2.
